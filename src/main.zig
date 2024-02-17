5b6550f52d59c04d029150117a53d6e5c4aa7775
Andreas Stocker <xflow-systems@proton.me>
2024-02-17 14:03:15 +0100

    ts_highlighter_new does not segfault

--- src/main.zig
+++ src/main.zig
@@ -9,6 +9,20 @@ const c = @cImport({
 
 extern "c" fn tree_sitter_zig() *c.TSLanguage;
 
+const CStrResult = struct {
+    bytes: [*c]const u8,
+    len: u32,
+};
+
+fn cStr(comptime string: []const u8) CStrResult {
+    const null_byte = "\x00";
+    return CStrResult{ .bytes = string ++ null_byte, .len = string.len + 1 };
+}
+
+fn csb(comptime string: []const u8) [*c]const u8 {
+    return cStr(string).bytes;
+}
+
 pub fn main() !void {
     // const parser: ?*c.TSParser = c.ts_parser_new();
     // defer c.ts_parser_delete(parser);
@@ -21,29 +35,33 @@ pub fn main() !void {
     // const root_node = c.ts_tree_root_node(tree);
     // std.debug.print("syntax tree: {s}\n", .{c.ts_node_string(root_node)});
 
-    const names: []const []const u8 = &.{ "function", "type", "constant", "keyword", "string" };
-    const attribute_strings: []const []const u8 = &.{ "", "", "", "", "" };
+    // const names: []const []const u8 = &.{ "function", "type", "constant", "keyword", "string" };
+    // const attribute_strings: []const []const u8 = &.{ "", "", "", "", "" };
+
+    const names: []const [*c]const u8 = &.{ csb("function"), csb("type"), csb("constant"), csb("keyword"), csb("string") };
+    const attribute_strings: []const [*c]const u8 = &.{ csb(""), csb(""), csb(""), csb(""), csb("") };
 
     // todo replace 1 with 5 / names.len
-    const highlighter = c.ts_highlighter_new(@constCast(@ptrCast(names)), @constCast(@ptrCast(attribute_strings)), 0);
+    const highlighter = c.ts_highlighter_new(@constCast(@ptrCast(names)), @constCast(@ptrCast(attribute_strings)), @truncate(names.len));
+    _ = highlighter;
 
-    const scope = "source.zig";
-    const injection_regex = "^zig";
-    const language = tree_sitter_zig();
-    const highlights_query = @embedFile("queries/highlights.scm");
-    const injections_query = @embedFile("queries/injections.scm");
-    const locals_query = "";
+    // const scope = cStr("source.zig");
+    // const injection_regex = cStr("^zig");
+    // const language = tree_sitter_zig();
+    // const highlights_query = cStr(@embedFile("queries/highlights.scm"));
+    // const injections_query = cStr(@embedFile("queries/injections.scm"));
+    // const locals_query = cStr("");
 
-    _ = c.ts_highlighter_add_language(
-        highlighter,
-        scope,
-        injection_regex,
-        language,
-        highlights_query,
-        injections_query,
-        locals_query,
-        highlights_query.len,
-        injections_query.len,
-        locals_query.len,
-    );
+    // _ = c.ts_highlighter_add_language(
+    //     highlighter,
+    //     scope.bytes,
+    //     injection_regex.bytes,
+    //     language,
+    //     highlights_query.bytes,
+    //     injections_query.bytes,
+    //     locals_query.bytes,
+    //     highlights_query.len,
+    //     injections_query.len,
+    //     locals_query.len,
+    // );
 }
